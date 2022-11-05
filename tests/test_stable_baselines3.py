6bdf51c4743b08a5546d003ec7951c82d1ebcf3f
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2022-11-06 00:19:42 +0100

    running version

--- tests/test_stable_baselines3.py
+++ tests/test_stable_baselines3.py
@@ -74,29 +74,27 @@ def test_piston():
         act = model.predict(obs, deterministic=True)[0] if not done else None
         env.step(act)
         env.render()
+
+
 import gym
 import numpy as np
 import supersuit as ss
 from pettingzoo.mpe import simple_spread_v2
-from pettingzoo.utils.conversions import (
-    aec_to_parallel,
-    turn_based_aec_to_parallel_wrapper,
-)
+
 
 def test_simple_spread():
-    
 
     env = simple_spread_v2.parallel_env()
     # env = aec_to_parallel(env)
-    env = ss.pettingzoo_env_to_vec_env_v1(env)
-    env = ss.concat_vec_envs_v1(env, 1, num_cpus=1, base_class="stable_baselines3")
+    env = ss.pettingzoo_env_to_vec_env_v0(env)
+    env = ss.concat_vec_envs_v0(env, 1, num_cpus=1, base_class="stable_baselines3")
 
     # check_env(env)
 
     # Fix conversion gymnasium usage in petitngzoo but gym in PR fixed sb3
-    env.action_space = gym.spaces.discrete.Discrete(5)
-    env.observation_space = gym.spaces.box.Box(-np.inf, np.inf, (18,), np.float32)
-    print(env.__dict__)
+    # env.action_space = gym.spaces.discrete.Discrete(5)
+    # env.observation_space = gym.spaces.box.Box(-np.inf, np.inf, (18,), np.float32)
+    # print(env.__dict__)
 
     model = PPO("MlpPolicy", env, verbose=1)
     model.learn(total_timesteps=10_000)
@@ -104,28 +102,32 @@ def test_simple_spread():
     obs = env.reset()
     for i in range(1000):
         action, _state = model.predict(obs, deterministic=True)
-    obs, reward, termination, truncation, info = env.step(action)
-    env.render()
-    if done:
-        obs = env.reset()
+        obs, reward, done, info = env.step(action)
+        env.render()
+        if done.any():
+            obs = env.reset()
+
 
 def test_sb():
-    env = simple_spread_v2.parallel_env(N=2, local_ratio=0.5,max_cycles=10, continuous_actions=False)
+    env = simple_spread_v2.parallel_env(
+        N=2, local_ratio=0.5, max_cycles=10, continuous_actions=False
+    )
     env = ss.pettingzoo_env_to_vec_env_v1(env)
-    env = ss.concat_vec_envs_v1(env, 1, num_cpus=4,base_class='stable_baselines3')
-    
+    env = ss.concat_vec_envs_v1(env, 1, num_cpus=4, base_class="stable_baselines3")
+
     expert = PPO(
-            policy="MlpPolicy",
-            env=env,
-            batch_size=64,
-            ent_coef=0.0,
-            learning_rate=0.0003,
-            n_epochs=10,
-            n_steps=64,
-            tensorboard_log = './gail_single_gen',
-        )
+        policy="MlpPolicy",
+        env=env,
+        batch_size=64,
+        ent_coef=0.0,
+        learning_rate=0.0003,
+        n_epochs=10,
+        n_steps=64,
+        tensorboard_log="./gail_single_gen",
+    )
     expert.learn(100000)  # Note: set to 100000 to train a proficient expert
-    #reward, r_std = evaluate_policy(expert, env, 50)
-    #print(reward,r_std)
+    # reward, r_std = evaluate_policy(expert, env, 50)
+    # print(reward,r_std)
+
 
 test_simple_spread()
